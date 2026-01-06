c423b9b8e2926ee2275e8ea5bac8f575938df855
Shreyasharma <shreya.sharma7110@gmail.com>
2026-01-06 18:14:29 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -6,7 +6,7 @@ import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.png'
 import profile_image1 from './profile_image1.png'
 
-export const url = 'http://localhost:4000'
+export const url = 'https://elaichi-2.onrender.com'
 export const currency = '₹'
 
 export const assets ={
